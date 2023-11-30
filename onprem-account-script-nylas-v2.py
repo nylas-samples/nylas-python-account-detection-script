08f9f715face64a6553a2fb0ac88364af2d7e264
Chase <chase.w@nylas.com>
2023-11-30 16:02:17 -0700

    Pushing script

---
+++ onprem-account-script-nylas-v2.py
@@ -0,0 +1,68 @@
+import requests
+import time
+import csv
+import base64
+
+# Configuration
+client_id = 'YOUR CLIENT ID'  # Replace with your actual client ID
+client_secret = 'YOUR CLIENT SECRET'  # Replace with your actual client secret
+api_endpoint = 'https://api.nylas.com/a/{}/accounts'.format(client_id)
+request_limit = 100  # Max accounts per request
+requests_per_second = 5  # Configurable rate of requests per second, max 10
+personal_microsoft_domains = ['outlook.com', 'hotmail.com', 'msn.com']  # List of personal Microsoft account providers
+
+# Base64 encode the client secret
+auth_string = base64.b64encode(f"{client_secret}:".encode()).decode()
+
+# Headers for the API request
+headers = {
+    'Authorization': f'Basic {auth_string}',
+    'Content-Type': 'application/json'
+}
+
+# Function to get accounts from Nylas
+def get_accounts(offset):
+    params = {
+        'limit': request_limit,
+        'offset': offset
+    }
+    response = requests.get(api_endpoint, headers=headers, params=params)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error fetching accounts: {response.text}")
+        return None
+
+# Main script
+def main():
+    offset = 0
+    all_accounts = []
+
+    while True:
+        accounts = get_accounts(offset)
+        if not accounts:
+            break
+
+        for account in accounts:
+            if account['provider'] == 'microsoft' and account['authentication_type'] == 'password':
+                domain = account['email_address'].split('@')[-1]
+                if domain not in personal_microsoft_domains:
+                    all_accounts.append(account)
+
+        offset += request_limit
+        time.sleep(1 / requests_per_second)
+
+        if len(accounts) < request_limit:
+            break
+
+    # Write to CSV
+    if(all_accounts.__len__() > 0):
+        with open('nylas_accounts.csv', 'w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=all_accounts[0].keys())
+            writer.writeheader()
+            writer.writerows(all_accounts)
+    else:
+        print("No accounts found")
+
+if __name__ == "__main__":
+    main()
---
+++ readme
@@ -0,0 +1,25 @@
+# Nylas Accounts Extractor
+
+This Python script, developed by `wallacecs007`, interfaces with the Nylas API to extract account information, specifically targeting Microsoft accounts with password authentication, excluding personal account domains like Outlook, Hotmail, and MSN. The extracted data is then stored in a CSV file for further analysis or use.
+
+## Features
+
+- **API Integration**: Connects to the Nylas API to fetch account data.
+- **Microsoft Account Filtering**: Filters out Microsoft accounts with 'password' authentication type.
+- **Domain Exclusion**: Excludes personal Microsoft account domains.
+- **CSV Export**: Outputs relevant account data to a CSV file.
+
+## Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+- Python 3.6 or higher installed on your system.
+- The `requests` library installed. You can install it using `pip install requests`.
+
+## Installation
+
+Clone the repository to your local machine:
+
+```bash
+git clone https://github.com/wallacecs007/nylas-accounts-extractor.git
+cd nylas-accounts-extractor
